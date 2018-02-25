8c952ee4eec2153ef920f626a389f748e95ffccc
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-02-25 16:19:17 +0100

    enhanced argparsing

--- contributors2ids.py
+++ contributors2ids.py
@@ -1,5 +1,6 @@
 import json
 import argparse
+import sys
 from mw import xml_dump
 
 
@@ -23,8 +24,8 @@ def create_mappings(input_dump_files):
     
 # TODO print durch logging ersetzen? (führt zu Problemen bei Windows mit multiprocessing)
 def main():  
-    parser = argparse.ArgumentParser(description='extracts contributors from given wikimedia dumps and creates a contributor->id mapping JSON file')
-    parser.add_argument("input_dump_files", nargs='+', type=argparse.FileType('r'), help='paths to wikimedia history dumps (such as "afwiki-20070124-pages-meta-history.xml.bz2")')
+    parser = argparse.ArgumentParser(description='extracts contributors from given wikimedia dumps and creates a contributor->id mapping JSON file', epilog='Example: ./{} collections/afwiki-20070124-pages-meta-history.xml.bz2 res.json'.format(sys.argv[0]))
+    parser.add_argument("input_dump_files", nargs='+', type=argparse.FileType('r'), help='paths to wikimedia history dumps (.xml or .xml.bz2)')
     parser.add_argument("output_mappings_file", type=argparse.FileType('w'), help='path to resulting contributor->id mappings file')
     args = parser.parse_args()
     input_dump_files = [file.name for file in args.input_dump_files]

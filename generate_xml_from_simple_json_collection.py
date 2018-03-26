90210d5cb2e19399bf186ec0a78cc42f9e9e47d0
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-03-26 17:23:42 +0200

    added namespace 0 to simple collection

--- generate_xml_from_simple_json_collection.py
+++ generate_xml_from_simple_json_collection.py
@@ -8,32 +8,16 @@ from collections import OrderedDict
 
 
 def generate_xmldump_base_object():
-    base_xml = '<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.3/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.3/ http://www.mediawiki.org/xml/export-0.3.xsd" version="0.3" xml:lang="af"> \
-        <siteinfo> \
-            <sitename>Wikipedia</sitename>\
-            <base>http://en.wikipedia.org/wiki/Main_Page</base>\
-            <generator>MediaWiki 1.10alpha</generator>\
-            <case>first-letter</case>\
-            <namespaces>\
-            <namespace key="-2" case="first-letter">Media</namespace>\
-            <namespace key="-1" case="first-letter">Special</namespace>\
+    base_xml = '<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.8/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.8/ http://www.mediawiki.org/xml/export-0.8.xsd" version="0.8" xml:lang="en">\
+    <siteinfo>\
+        <sitename>Wikipedia</sitename>\
+        <base>http://en.wikipedia.org/wiki/Main_Page</base>\
+        <generator>MediaWiki 1.22wmf2</generator>\
+        <case>first-letter</case>\
+        <namespaces>\
             <namespace key="0" case="first-letter" />\
             <namespace key="1" case="first-letter">Talk</namespace>\
-            <namespace key="2" case="first-letter">User</namespace>\
-            <namespace key="3" case="first-letter">User talk</namespace>\
-            <namespace key="4" case="first-letter">Wikipedia</namespace>\
-            <namespace key="5" case="first-letter">Wikipedia talk</namespace>\
-            <namespace key="6" case="first-letter">File</namespace>\
-            <namespace key="7" case="first-letter">File talk</namespace>\
-            <namespace key="8" case="first-letter">MediaWiki</namespace>\
-            <namespace key="9" case="first-letter">MediaWiki talk</namespace>\
-            <namespace key="10" case="first-letter">Template</namespace>\
-            <namespace key="11" case="first-letter">Template talk</namespace>\
-            <namespace key="12" case="first-letter">Help</namespace>\
-            <namespace key="13" case="first-letter">Help talk</namespace>\
-            <namespace key="14" case="first-letter">Category</namespace>\
-            <namespace key="15" case="first-letter">Category talk</namespace>\
-            </namespaces>\
+        </namespaces>\
         </siteinfo>\
     </mediawiki>'
     return xmltodict.parse(base_xml)
@@ -51,6 +35,7 @@ def create_revisions_pages_from_simple_collection(simple_collection, start_date=
     for docid,doc in simple_collection.items():
         page = OrderedDict()
         page['title'] = 'Document {}'.format(docid)
+        page['ns'] = 0
         page['id'] = docid
         page['revision'] = []
         for i,rev in enumerate(doc['revisions']):
@@ -114,7 +99,37 @@ def main():
 if __name__ == '__main__':
     main()
     
-    
+
+    """
+    base_xml = '<mediawiki xmlns="http://www.mediawiki.org/xml/export-0.3/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.mediawiki.org/xml/export-0.3/ http://www.mediawiki.org/xml/export-0.3.xsd" version="0.3" xml:lang="af"> \
+        <siteinfo> \
+            <sitename>Wikipedia</sitename>\
+            <base>http://en.wikipedia.org/wiki/Main_Page</base>\
+            <generator>MediaWiki 1.10alpha</generator>\
+            <case>first-letter</case>\
+            <namespaces>\
+            <namespace key="-2" case="first-letter">Media</namespace>\
+            <namespace key="-1" case="first-letter">Special</namespace>\
+            <namespace key="0" case="first-letter" />\
+            <namespace key="1" case="first-letter">Talk</namespace>\
+            <namespace key="2" case="first-letter">User</namespace>\
+            <namespace key="3" case="first-letter">User talk</namespace>\
+            <namespace key="4" case="first-letter">Wikipedia</namespace>\
+            <namespace key="5" case="first-letter">Wikipedia talk</namespace>\
+            <namespace key="6" case="first-letter">File</namespace>\
+            <namespace key="7" case="first-letter">File talk</namespace>\
+            <namespace key="8" case="first-letter">MediaWiki</namespace>\
+            <namespace key="9" case="first-letter">MediaWiki talk</namespace>\
+            <namespace key="10" case="first-letter">Template</namespace>\
+            <namespace key="11" case="first-letter">Template talk</namespace>\
+            <namespace key="12" case="first-letter">Help</namespace>\
+            <namespace key="13" case="first-letter">Help talk</namespace>\
+            <namespace key="14" case="first-letter">Category</namespace>\
+            <namespace key="15" case="first-letter">Category talk</namespace>\
+            </namespaces>\
+        </siteinfo>\
+    </mediawiki>'
+    """
     
     
     
--- run_simple_collection.sh
+++ run_simple_collection.sh
@@ -6,16 +6,20 @@ OUT_PREFIX="output/$PREFIX"
 
 echo "generating XML dumps from JSON description"
 time python generate_xml_from_simple_json_collection.py "$PREFIX.json" "$COLL_PREFIX-articles.xml" "$COLL_PREFIX-pages-meta-history.xml"
-echo "generating JSON revdocs from XML dumps"
-time ~/Python-Miniconda3/Scripts/mwxml.exe dump2revdocs "$COLL_PREFIX-pages-meta-history.xml" --output="$OUT_PREFIX-revdocs" --compress="json" --verbose
-echo "generating JSON revdocs with SHA1 hashes from revision texts"
-time python ~/Eclipse-Projekte/Wikipedia-Cluster-Analysis/add_sha1_to_revdocs.py "$OUT_PREFIX-revdocs/$PREFIX-pages-meta-history.json" --compress="json" "$OUT_PREFIX-revdocs-sha1"  
-echo "generating diffs from revdocs"
-# TODO hier namespaces filetern? oder in Zwischenschritt ganz kicken?
-time ~/Python-Miniconda3/Scripts/mwdiffs.exe revdocs2diffs "$OUT_PREFIX-revdocs-sha1/$PREFIX-pages-meta-history.json" --config="simple_collection_diffengine_config.yaml" --output="$OUT_PREFIX-diffs" --compress="json" --verbose
-echo "calculating persistence data from diffs"
-time ~/Python-Miniconda3/Scripts/mwpersistence.exe diffs2persistence "$OUT_PREFIX-diffs/$PREFIX-pages-meta-history.json" --output="$OUT_PREFIX-persistence" --sunset="<now>" --compres="json" --verbose 
-echo "calculating stats from persistence"
-time ~/Python-Miniconda3/Scripts/mwpersistence.exe persistence2stats "$OUT_PREFIX-persistence/$PREFIX-pages-meta-history.json" --output="$OUT_PREFIX-stats" --compress="json" --min-visible=0 --verbose
-python display_stats.py "$OUT_PREFIX-stats/$PREFIX-pages-meta-history.json" 
+# echo "generating JSON revdocs from XML dumps"
+# time ~/Python-Miniconda3/Scripts/mwxml.exe dump2revdocs "$COLL_PREFIX-pages-meta-history.xml" --output="$OUT_PREFIX-revdocs" --compress="json" --verbose
+# echo "generating JSON revdocs with SHA1 hashes from revision texts"
+# time python ~/Eclipse-Projekte/Wikipedia-Cluster-Analysis/add_sha1_to_revdocs.py "$OUT_PREFIX-revdocs/$PREFIX-pages-meta-history.json" --compress="json" "$OUT_PREFIX-revdocs-sha1"  
+# echo "generating diffs from revdocs"
+# #TODO hier namespaces filetern? oder in Zwischenschritt ganz kicken?
+# time ~/Python-Miniconda3/Scripts/mwdiffs.exe revdocs2diffs "$OUT_PREFIX-revdocs-sha1/$PREFIX-pages-meta-history.json" --config="simple_collection_diffengine_config.yaml" --output="$OUT_PREFIX-diffs" --compress="json" --verbose
+# echo "calculating persistence data from diffs"
+# time ~/Python-Miniconda3/Scripts/mwpersistence.exe diffs2persistence "$OUT_PREFIX-diffs/$PREFIX-pages-meta-history.json" --output="$OUT_PREFIX-persistence" --sunset="<now>" --compres="json" --verbose 
+# echo "calculating stats from persistence"
+# time ~/Python-Miniconda3/Scripts/mwpersistence.exe persistence2stats "$OUT_PREFIX-persistence/$PREFIX-pages-meta-history.json" --output="$OUT_PREFIX-stats" --compress="json" --min-visible=0 --verbose
+# python display_stats.py "$OUT_PREFIX-stats/$PREFIX-pages-meta-history.json" 
 
+bzip2 -zkf "$COLL_PREFIX-articles.xml"
+bzip2 -zkf "$COLL_PREFIX-pages-meta-history.xml"
+# VOCABULARY_SIZE=100000
+python -m gensim.scripts.make_wiki "$COLL_PREFIX-articles.xml.bz2" "$OUT_PREFIX-tm" $VOCABULARY_SIZE

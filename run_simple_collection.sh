e966dc0aec1c16e6d83c4737ab59dcb6f08d6a9c
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-03-28 10:29:35 +0200

    added namespaces parameter to simple_make_wiki

--- run_simple_collection.sh
+++ run_simple_collection.sh
@@ -7,6 +7,25 @@ TM_DIR="output/topic"
 
 echo "generating XML dumps from JSON description"
 time python generate_xml_from_simple_json_collection.py "$PREFIX.json" "$COLL_PREFIX-articles.xml" "$COLL_PREFIX-pages-meta-history.xml"
+
+# TODO preprocessing (stopwords,...)
+
+# erfordert grundsätzlich bz2
+bzip2 -zkf "$COLL_PREFIX-articles.xml"
+bzip2 -zkf "$COLL_PREFIX-pages-meta-history.xml"
+mkdir -p $TM_DIR
+VOCABULARY_SIZE=100
+NO_BELOW=0
+NO_ABOVE=1.0
+ARTICLE_MIN_TOKENS=1
+TOKEN_MIN_LEN=2
+TOKEN_MAX_LEN=20
+NAMESPACES="0"
+#python -m gensim.scripts.make_wiki "$COLL_PREFIX-articles.xml.bz2" "$TM_DIR/$PREFIX" $VOCABULARY_SIZE online
+echo "generating gensim model data"
+python simple_make_wiki.py "$COLL_PREFIX-articles.xml.bz2" "$TM_DIR/$PREFIX" --keep-words $VOCABULARY_SIZE --no-below=$NO_BELOW --no-above=$NO_ABOVE --article-min-tokens $ARTICLE_MIN_TOKENS --token-min-len $TOKEN_MIN_LEN --token-max-len $TOKEN_MAX_LEN --namespaces $NAMESPACES
+
+
 # echo "generating JSON revdocs from XML dumps"
 # time ~/Python-Miniconda3/Scripts/mwxml.exe dump2revdocs "$COLL_PREFIX-pages-meta-history.xml" --output="$OUT_PREFIX-revdocs" --compress="json" --verbose
 # echo "generating JSON revdocs with SHA1 hashes from revision texts"
@@ -19,20 +38,5 @@ time python generate_xml_from_simple_json_collection.py "$PREFIX.json" "$COLL_PR
 # echo "calculating stats from persistence"
 # time ~/Python-Miniconda3/Scripts/mwpersistence.exe persistence2stats "$OUT_PREFIX-persistence/$PREFIX-pages-meta-history.json" --output="$OUT_PREFIX-stats" --compress="json" --min-visible=0 --verbose
 # python display_stats.py "$OUT_PREFIX-stats/$PREFIX-pages-meta-history.json" 
-
-# erfordert grundsätzlich bz2
-bzip2 -zkf "$COLL_PREFIX-articles.xml"
-bzip2 -zkf "$COLL_PREFIX-pages-meta-history.xml"
-mkdir -p $TM_DIR
-VOCABULARY_SIZE=100
-NO_BELOW=0
-NO_ABOVE=1.0
-ARTICLE_MIN_TOKENS=1
-TOKEN_MIN_LEN=2
-TOKEN_MAX_LEN=20
-#python -m gensim.scripts.make_wiki "$COLL_PREFIX-articles.xml.bz2" "$TM_DIR/$PREFIX" $VOCABULARY_SIZE online
-echo "generating gensim model data"
-python simple_make_wiki.py "$COLL_PREFIX-articles.xml.bz2" "$TM_DIR/$PREFIX" --keep-words $VOCABULARY_SIZE --no-below=$NO_BELOW --no-above=$NO_ABOVE --article-min-tokens $ARTICLE_MIN_TOKENS --token-min-len $TOKEN_MIN_LEN --token-max-len $TOKEN_MAX_LEN
-
 
 
--- simple_make_wiki.py
+++ simple_make_wiki.py
@@ -13,9 +13,10 @@ DEFAULT_NO_ABOVE = 0.5
 DEFAULT_ART_MIN_TOKENS = 50
 DEFAULT_TOKEN_MIN_LEN = 2
 DEFAULT_TOKEN_MAX_LEN = 20
+DEFAULT_NAMESPACES = 0
 
 def main():
-    parser = argparse.ArgumentParser(description='creates gensim data files from a given XML.BZ2 MediaWiki dump', epilog='Example: ./{} enwiki-pages-articles.xml.bz2 topicdata/enwiki --dict-size 100000'.format(sys.argv[0]))
+    parser = argparse.ArgumentParser(description='creates gensim data files from a given XML.BZ2 MediaWiki dump', epilog='Example: ./{} enwiki-pages-articles.xml.bz2 topicdata/enwiki --keep-words 1000 --no-below=10 --no-above=0.5 --article-min-tokens 50 --token-min-len 2 --token-max-len 20 --namespaces 0 '.format(sys.argv[0]))
     parser.add_argument("idump", type=argparse.FileType('r'), help='path to input XML.BZ2 articles dump')
     parser.add_argument("oprefix", help='prefix for generated gensim files')
     parser.add_argument("--keep-words", type=int, default=DEFAULT_DICT_SIZE, help='number of most frequent word types to keep (default {})'.format(DEFAULT_DICT_SIZE))
@@ -24,6 +25,7 @@ def main():
     parser.add_argument("--article-min-tokens", type=int, default=DEFAULT_ART_MIN_TOKENS, help='Analyze only articles of >= ARTICLE_MIN_TOKENS tokens default {}). Should be >=1'.format(DEFAULT_ART_MIN_TOKENS))
     parser.add_argument("--token-min-len", type=int, default=DEFAULT_TOKEN_MIN_LEN, help='Consider only tokens of at least TOKEN_MIN_LEN chars (default {})'.format(DEFAULT_TOKEN_MIN_LEN))
     parser.add_argument("--token-max-len", type=int, default=DEFAULT_TOKEN_MAX_LEN, help='Consider only tokens of at most TOKEN_MAX_LEN chars (default {})'.format(DEFAULT_TOKEN_MAX_LEN))
+    parser.add_argument("--namespaces", nargs='+', type=int, default=(DEFAULT_NAMESPACES), help='Consider only given MediaWiki namespaces (default {})'.format(DEFAULT_NAMESPACES))
     
     
     args = parser.parse_args()
@@ -33,6 +35,8 @@ def main():
     no_below,no_above = args.no_below,args.no_above
     article_min_tokens = args.article_min_tokens
     token_min_len,token_max_len = args.token_min_len,args.token_max_len
+    namespaces = tuple(str(ns) for ns in args.namespaces)
+    
     if not os.path.isdir(os.path.dirname(output_files_prefix)):
         parser.print_help(sys.stderr)
         raise SystemExit('Error: Directory "{}" not exits. Create the directory and try again.'.format(os.path.dirname(output_files_prefix)))
@@ -42,9 +46,10 @@ def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')    
     logging.root.level = logging.DEBUG
     
+    logger.info('running {} with input_articles {}, output_prefix {}, keep_words {}, no_below {}, no_above {}, article_min_tokens {}, token_min_len {}, token_max_len {}, namespaces {}'.format(sys.argv[0],input_articles_path,output_files_prefix,keep_words,no_below,no_above,article_min_tokens,token_min_len,token_max_len,namespaces))
     dictionary = HashDictionary(id_range=keep_words, debug=False)
     dictionary.allow_update = True  # start collecting document frequencies
-    wiki = WikiCorpus(input_articles_path, lemmatize=False, dictionary=dictionary, article_min_tokens=article_min_tokens, token_min_len=token_min_len, token_max_len=token_max_len)
+    wiki = WikiCorpus(input_articles_path, lemmatize=False, dictionary=dictionary, article_min_tokens=article_min_tokens, token_min_len=token_min_len, token_max_len=token_max_len, filter_namespaces=namespaces)
     MmCorpus.serialize(output_files_prefix + '_bow.mm', wiki, progress_cnt=10000)
     dictionary.filter_extremes(no_below=no_below, no_above=no_above, keep_n=keep_words) # im Original stand hier DEFAULT_DICT_SIZE?
     dictionary.save_as_text(output_files_prefix + '_wordids.txt.bz2')

f01ae503c9a1bada9f76d8e7f28b3f90a192387e
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-04-02 18:12:16 +0200

    added kmeans parameter batch size

--- run_simple_collection.sh
+++ run_simple_collection.sh
@@ -60,8 +60,9 @@ python scripts/utils/apply_lda_model_to_corpus.py $BOW_PREFIX-corpus.mm.bz2 $TM_
 
 mkdir -p $CLUS_DIR
 NUMCLUSTERS=$NUMTOPICS
+BATCHSIZE=1000
 echo "computing kmeans clusters"
-time python scripts/run_kmeans.py $BOW_PREFIX-corpus.mm.bz2 $TM_PREFIX-lda-model $CLUS_PREFIX-kmeans-labels.cpickle.bz2 $NUMCLUSTERS 2>&1 | tee $LOG_PREFIX-kmeans.log
+time python scripts/run_kmeans.py $BOW_PREFIX-corpus.mm.bz2 $TM_PREFIX-lda-model $CLUS_PREFIX-kmeans-labels.cpickle.bz2 $NUMCLUSTERS --batch-size=$BATCHSIZE 2>&1 | tee $LOG_PREFIX-kmeans.log
 python scripts/utils/binary_to_text.py numpy $CLUS_PREFIX-kmeans-labels.cpickle.bz2 $CLUS_PREFIX-kmeans-labels.txt # TODO produktiv raus
 
 
--- scripts/run_kmeans.py
+++ scripts/run_kmeans.py
@@ -10,6 +10,7 @@ from sklearn.cluster import MiniBatchKMeans
 from utils.utils import init_gensim_logger
 import numpy as np
  
+DEFAULT_BATCH_SIZE = 1000
 
 def main():
     parser = argparse.ArgumentParser(description='clusters documents of a corpus with k-means by applying a trained lda model to the corpus', epilog='Example: ./{} mycorpus-bow.mm.bz2 mycorpus-lda-model mycorpus-clusters.cpickle.bz2 --id2word mycorpus-wordids.txt.bz2 --passes 10 --iterations 100 '.format(sys.argv[0]))
@@ -17,28 +18,30 @@ def main():
     parser.add_argument('lda', type=argparse.FileType('r'), help='path to binary input lda model file')
     parser.add_argument('cluster_labels', type=argparse.FileType('w'), help='path to output binary cluster labels file (.cpickle/.cpickle.bz2)')
     parser.add_argument('numclusters', type=int, help='number of clusters to create')
+    parser.add_argument('--batch-size', type=int, default=DEFAULT_BATCH_SIZE, help='size of mini batches (default {})'.format(DEFAULT_BATCH_SIZE))
+    
     args = parser.parse_args()
     input_corpus_path = args.corpus.name
     input_lda_path = args.lda.name
     output_cluster_labels_path = args.cluster_labels.name
     numclusters = args.numclusters
+    batch_size = args.batch_size
     
     program, logger = init_gensim_logger()  
-    logger.info('running {} with:\n{}'.format(program, pformat({'input_corpus_path':input_corpus_path, 'input_lda_path':input_lda_path, 'output_cluster_labels_path':output_cluster_labels_path, 'numclusters':numclusters})))
+    logger.info('running {} with:\n{}'.format(program, pformat({'input_corpus_path':input_corpus_path, 'input_lda_path':input_lda_path, 'output_cluster_labels_path':output_cluster_labels_path, 'numclusters':numclusters, 'batch_size':batch_size})))
         
     corpus = MmCorpus(input_corpus_path)
     lda = LdaMulticore.load(input_lda_path)
     dense = corpus2dense(lda[corpus], lda.num_topics, corpus.num_docs).T  # TODO das wird nicht gestreamt -> probleme?
     verbose = 'DEBUG' in os.environ
-    kmeans = MiniBatchKMeans(n_clusters=numclusters, init='k-means++', max_iter=1000000, batch_size=100, verbose=verbose, compute_labels=True, tol=0.0, max_no_improvement=10, n_init=3, reassignment_ratio=0.01)
+    kmeans = MiniBatchKMeans(n_clusters=numclusters, init='k-means++', max_iter=1000000, batch_size=batch_size, verbose=verbose, compute_labels=True)
     logger.info('running k-means on {} documents, {} topics -> {} clusters'.format(corpus.num_docs,lda.num_topics,numclusters))
     logger.info(kmeans)
     cluster_labels = kmeans.fit_predict(dense)
     logger.info('labels')
-    logger.info(type(cluster_labels))
+    logger.info(cluster_labels)
     with smart_open(output_cluster_labels_path, 'wb') as ofile:
         np.save(ofile, cluster_labels)
-    #np.savetxt(output_cluster_labels_path + '.txt', cluster_labels) # TODO entfernen
     
     logger.info("finished running %s", program)
     

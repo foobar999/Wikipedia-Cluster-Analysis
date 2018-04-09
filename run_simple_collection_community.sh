ee8947dd8071130a3f0b676e42c3c56dda026046
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-04-09 15:10:36 +0200

    added run_simple_collection_community.sh

---
+++ run_simple_collection_community.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e  # Abbruch bei Fehler
+export DEBUG="DEBUG" # TODO produktiv raus
+PREFIX="simple-collection"
+COLL_PREFIX="collections/$PREFIX"
+mkdir -p "output/contribs"
+CONTRIB_PREFIX="output/contribs/$PREFIX"
+mkdir -p "output/logs"
+LOG_PREFIX="output/logs/$PREFIX"
+
+echo "generating XML dumps from JSON description"
+time python scripts/utils/generate_xml_from_simple_json_collection.py $PREFIX.json $COLL_PREFIX-articles.xml $COLL_PREFIX-pages-meta-history.xml
+bzip2 -zkf $COLL_PREFIX-articles.xml $COLL_PREFIX-pages-meta-history.xml # gensim erfordert grundsätzlich .xml.bz2-Dateien
+
+echo "computing id2author dictionary"
+( time python scripts/history_to_id2author.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --id2author=$CONTRIB_PREFIX-id2author.cpickle.bz2 ) |& tee $LOG_PREFIX-id2author.log
+python scripts/utils/binary_to_text.py gensim $CONTRIB_PREFIX-id2author.cpickle.bz2 $CONTRIB_PREFIX-id2author.txt # TODO produktiv raus
+

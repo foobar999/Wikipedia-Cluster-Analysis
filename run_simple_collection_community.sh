b15c24bf89b704d160b9306bbe37224e1375bea0
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-04-13 19:48:34 +0200

    added viz gen to simple-collection

--- run_simple_collection_community.sh
+++ run_simple_collection_community.sh
@@ -41,6 +41,13 @@ bzip2 -zkf $COLL_PREFIX-articles.xml $COLL_PREFIX-pages-meta-history.xml
 
 echo "calculating stats from history dump"
 ( time python scripts/get_history_stats.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --stat-files-prefix=$STATS_PREFIX ) |& tee $LOG_PREFIX-stats.log
+QUANTILE=0.99
+for STAT_FILE in $STATS_PREFIX*.csv; do
+    [ -f "$STAT_FILE" ] || break
+    IMAGE_FILE="${STAT_FILE%%.*}.pdf"
+    LOG_FILE="$(basename ${STAT_FILE%%.*})"
+    ( python scripts/visualize_stats.py --stats=$STAT_FILE --viz=$IMAGE_FILE --quantile=$QUANTILE ) |& tee output/logs/$LOG_FILE.log
+done
 
 echo "computing author contributions"
 CONTRIBUTION_VALUE=diff_numterms

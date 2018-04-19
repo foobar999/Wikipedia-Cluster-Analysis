0163a8d934aff4103db0be408b159757a118b42e
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-04-19 21:43:57 +0200

    reduced no of log files by tee -a

--- run_history_to_contribs.sh
+++ run_history_to_contribs.sh
@@ -11,11 +11,11 @@ LOG_PREFIX="output/logs/$PREFIX"
 echo "computing author contributions"
 CONTRIBUTION_VALUE=one
 MIN_AUTH_DOCS=2
-( time python scripts/history_to_contribs.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --id2author=$CONTRIB_PREFIX-id2author.cpickle.bz2 --contribs=$CONTRIB_PREFIX-raw-contributions.mm --contribution-value=$CONTRIBUTION_VALUE --min-auth-docs=$MIN_AUTH_DOCS --namespace-prefixes=output/$PREFIX-namespaces.txt) |& tee $LOG_PREFIX-raw-contribs.log
+( time python scripts/history_to_contribs.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --id2author=$CONTRIB_PREFIX-id2author.cpickle.bz2 --contribs=$CONTRIB_PREFIX-raw-contributions.mm --contribution-value=$CONTRIBUTION_VALUE --min-auth-docs=$MIN_AUTH_DOCS --namespace-prefixes=output/$PREFIX-namespaces.txt) |& tee $LOG_PREFIX-contribs.log
 mv $CONTRIB_PREFIX-raw-contributions.mm.metadata.cpickle $CONTRIB_PREFIX.titles.cpickle # Artikeltitel-Datei umbennen
 bzip2 -zf $CONTRIB_PREFIX-raw-contributions.mm $CONTRIB_PREFIX.titles.cpickle
 
 echo "accmulating contributions"
-( time python scripts/accumulate_contribs.py --raw-contribs=$CONTRIB_PREFIX-raw-contributions.mm.bz2 --acc-contribs=$CONTRIB_PREFIX-acc-contributions.mm ) |& tee $LOG_PREFIX-acc-contribs.log
+( time python scripts/accumulate_contribs.py --raw-contribs=$CONTRIB_PREFIX-raw-contributions.mm.bz2 --acc-contribs=$CONTRIB_PREFIX-acc-contributions.mm ) |& tee -a $LOG_PREFIX-contribs.log
 bzip2 -zf $CONTRIB_PREFIX-acc-contributions.mm
 
--- run_simple_collection_community.sh
+++ run_simple_collection_community.sh
@@ -47,7 +47,7 @@ NS_MIN_OCCURENCES=1
 echo "computing author contributions"
 CONTRIBUTION_VALUE=diff_numterms
 MIN_AUTH_DOCS=1
-( time python scripts/history_to_contribs.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --id2author=$CONTRIB_PREFIX-id2author.cpickle.bz2 --contribs=$CONTRIB_PREFIX-raw-contributions.mm --contribution-value=$CONTRIBUTION_VALUE --min-auth-docs=$MIN_AUTH_DOCS --namespace-prefixes=$NAMESPACE_PREFIXES_FILE ) |& tee $LOG_PREFIX-raw-contribs.log
+( time python scripts/history_to_contribs.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --id2author=$CONTRIB_PREFIX-id2author.cpickle.bz2 --contribs=$CONTRIB_PREFIX-raw-contributions.mm --contribution-value=$CONTRIBUTION_VALUE --min-auth-docs=$MIN_AUTH_DOCS --namespace-prefixes=$NAMESPACE_PREFIXES_FILE ) |& tee $LOG_PREFIX-contribs.log
 python scripts/utils/binary_to_text.py gensim $CONTRIB_PREFIX-id2author.cpickle.bz2 $CONTRIB_PREFIX-id2author.txt # TODO produktiv raus
 mv $CONTRIB_PREFIX-raw-contributions.mm.metadata.cpickle $CONTRIB_PREFIX.titles.cpickle # Artikeltitel-Datei umbennen
 bzip2 -zf $CONTRIB_PREFIX-raw-contributions.mm $CONTRIB_PREFIX.titles.cpickle # komprimiere Beiträge, Artikeltitel
@@ -55,10 +55,10 @@ bzip2 -dkf $CONTRIB_PREFIX-raw-contributions.mm.bz2  # TODO produktiv raus
 python scripts/utils/binary_to_text.py pickle $CONTRIB_PREFIX.titles.cpickle.bz2 $CONTRIB_PREFIX.titles.json # TODO produktiv raus
 
 echo "accmulating contributions"
-( time python scripts/accumulate_contribs.py --raw-contribs=$CONTRIB_PREFIX-raw-contributions.mm.bz2 --acc-contribs=$CONTRIB_PREFIX-doc-auth-contribs.mm ) |& tee $LOG_PREFIX-doc-auth-contribs.log
+( time python scripts/accumulate_contribs.py --raw-contribs=$CONTRIB_PREFIX-raw-contributions.mm.bz2 --acc-contribs=$CONTRIB_PREFIX-doc-auth-contribs.mm ) |& tee -a $LOG_PREFIX-contribs.log
 
 echo "transforming (docid,authorid,contribvalue) file to (authorid,docid,contribvalue) file"
-( time ./bash/get_swapped_author_doc_contribs.sh $CONTRIB_PREFIX-doc-auth-contribs.mm > $CONTRIB_PREFIX-auth-doc-contribs.mm) |& tee $LOG_PREFIX-auth-doc-contribs.log
+( time ./bash/get_swapped_author_doc_contribs.sh $CONTRIB_PREFIX-doc-auth-contribs.mm > $CONTRIB_PREFIX-auth-doc-contribs.mm) |& tee -a $LOG_PREFIX-contribs.log
 bzip2 -zf $CONTRIB_PREFIX-doc-auth-contribs.mm $CONTRIB_PREFIX-auth-doc-contribs.mm
 bzip2 -dkf $CONTRIB_PREFIX-doc-auth-contribs.mm.bz2 $CONTRIB_PREFIX-auth-doc-contribs.mm.bz2 # TODO produktiv raus
 

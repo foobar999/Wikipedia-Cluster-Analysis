b081b60b77b273bb4fb71fa51d597464fd013897
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-04-22 23:39:50 +0200

    added thresholding

--- run_simple_collection_community.sh
+++ run_simple_collection_community.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# TODO contributions -> contribs in anderen scripts
 # graph aufbau optimieren -> cocitation/bib coupling con igraph? https://gist.github.com/tts/4737987 http://igraph.org/python/doc/igraph.Graph-class.html#Incidence http://www.shizukalab.com/toolkits/sna/bipartite https://lists.nongnu.org/archive/html/igraph-help/2013-07/msg00014.html https://github.com/igraph/python-igraph/issues/168
 # TODO ip-adressen ignorieren
 # http://www.shizukalab.com/toolkits/sna/bipartite
@@ -49,20 +50,24 @@ echo "computing author contributions"
 CONTRIBUTION_VALUE=diff_numterms
 MIN_AUTH_DOCS=1
 MIN_DOC_AUTHS=1
-( time python scripts/history_to_contribs.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --id2author=$CONTRIB_PREFIX-id2author.cpickle.bz2 --contribs=$CONTRIB_PREFIX-raw-contributions.mm --contribution-value=$CONTRIBUTION_VALUE --min-auth-docs=$MIN_AUTH_DOCS --min-doc-auths=$MIN_DOC_AUTHS --namespace-prefixes=$NAMESPACE_PREFIXES_FILE ) |& tee $LOG_PREFIX-contribs.log
+( time python scripts/history_to_contribs.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --id2author=$CONTRIB_PREFIX-id2author.cpickle.bz2 --contribs=$CONTRIB_PREFIX-raw-contribs.mm --contribution-value=$CONTRIBUTION_VALUE --min-auth-docs=$MIN_AUTH_DOCS --min-doc-auths=$MIN_DOC_AUTHS --namespace-prefixes=$NAMESPACE_PREFIXES_FILE ) |& tee $LOG_PREFIX-contribs.log
 python scripts/utils/binary_to_text.py gensim $CONTRIB_PREFIX-id2author.cpickle.bz2 $CONTRIB_PREFIX-id2author.txt # TODO produktiv raus
-mv $CONTRIB_PREFIX-raw-contributions.mm.metadata.cpickle $CONTRIB_PREFIX.titles.cpickle # Artikeltitel-Datei umbennen
-bzip2 -zf $CONTRIB_PREFIX-raw-contributions.mm $CONTRIB_PREFIX.titles.cpickle # komprimiere Beiträge, Artikeltitel
-bzip2 -dkf $CONTRIB_PREFIX-raw-contributions.mm.bz2  # TODO produktiv raus
+mv $CONTRIB_PREFIX-raw-contribs.mm.metadata.cpickle $CONTRIB_PREFIX.titles.cpickle # Artikeltitel-Datei umbennen
+bzip2 -zf $CONTRIB_PREFIX-raw-contribs.mm $CONTRIB_PREFIX.titles.cpickle # komprimiere Beiträge, Artikeltitel
+bzip2 -dkf $CONTRIB_PREFIX-raw-contribs.mm.bz2  # TODO produktiv raus
 python scripts/utils/binary_to_text.py pickle $CONTRIB_PREFIX.titles.cpickle.bz2 $CONTRIB_PREFIX.titles.json # TODO produktiv raus
 
 echo "accmulating contributions"
-( time python scripts/accumulate_contribs.py --raw-contribs=$CONTRIB_PREFIX-raw-contributions.mm.bz2 --acc-contribs=$CONTRIB_PREFIX-doc-auth-contribs.mm ) |& tee -a $LOG_PREFIX-contribs.log
+( time python scripts/accumulate_contribs.py --raw-contribs=$CONTRIB_PREFIX-raw-contribs.mm.bz2 --acc-contribs=$CONTRIB_PREFIX-acc-contribs.mm ) |& tee -a $LOG_PREFIX-contribs.log
+
+echo "thresholding contributions"
+MIN_CONTRIB_VALUE=4
+./bash/threshold_contribs.sh $CONTRIB_PREFIX-acc-contribs.mm $MIN_CONTRIB_VALUE > $CONTRIB_PREFIX-doc-auth-contribs.mm
 
 echo "transforming (docid,authorid,contribvalue) file to (authorid,docid,contribvalue) file"
-( time ./bash/get_swapped_author_doc_contribs.sh $CONTRIB_PREFIX-doc-auth-contribs.mm > $CONTRIB_PREFIX-auth-doc-contribs.mm) |& tee -a $LOG_PREFIX-contribs.log
-bzip2 -zf $CONTRIB_PREFIX-doc-auth-contribs.mm $CONTRIB_PREFIX-auth-doc-contribs.mm
-bzip2 -dkf $CONTRIB_PREFIX-doc-auth-contribs.mm.bz2 $CONTRIB_PREFIX-auth-doc-contribs.mm.bz2 # TODO produktiv raus
+./bash/get_swapped_author_doc_contribs.sh $CONTRIB_PREFIX-doc-auth-contribs.mm > $CONTRIB_PREFIX-auth-doc-contribs.mm
+bzip2 -zf $CONTRIB_PREFIX-acc-contribs.mm $CONTRIB_PREFIX-doc-auth-contribs.mm $CONTRIB_PREFIX-auth-doc-contribs.mm
+bzip2 -dkf $CONTRIB_PREFIX-acc-contribs.mm.bz2 $CONTRIB_PREFIX-doc-auth-contribs.mm.bz2 $CONTRIB_PREFIX-auth-doc-contribs.mm.bz2 # TODO produktiv raus
 
 echo "creating graph from contributions"
 ( time python scripts/contribs_to_graph.py --contribs=$CONTRIB_PREFIX-auth-doc-contribs.mm.bz2 --graph=$GRAPH_PREFIX-co-authorship.cpickle.gz ) |& tee $LOG_PREFIX-graph.log

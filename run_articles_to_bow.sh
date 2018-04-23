cdfb28eed4837a997a0a5675fc1af559a2abd8f6
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-04-23 21:09:05 +0200

    comment

--- run_articles_to_bow.sh
+++ run_articles_to_bow.sh
@@ -23,6 +23,7 @@ NO_ABOVE=1
 ARTICLE_MIN_TOKENS=1
 TOKEN_MIN_LEN=1
 TOKEN_MAX_LEN=100
+# stopwords werden auch entfernt
 echo "generating bag-of-words corpus files"
 ( time python scripts/articles_to_bow.py --articles-dump=$COLL_PREFIX-pages-articles.xml.bz2 --out-prefix=$BOW_PREFIX-corpus --keep-words=$VOCABULARY_SIZE --no-below=$NO_BELOW --no-above=$NO_ABOVE --article-min-tokens=$ARTICLE_MIN_TOKENS --token-len-range $TOKEN_MIN_LEN $TOKEN_MAX_LEN --remove-stopwords --namespace-prefixes=output/$PREFIX-namespaces.txt ) |& tee $LOG_PREFIX-wiki-to-bow.log
 mv $BOW_PREFIX-corpus.mm.metadata.cpickle $BOW_PREFIX-corpus.metadata.cpickle # gib docID-Mapping intuitiveren Namen

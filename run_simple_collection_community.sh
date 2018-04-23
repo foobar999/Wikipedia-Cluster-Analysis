8e57d7a32b8065981fda6ae55ca87c5ba209d1f2
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-04-23 20:01:41 +0200

    refactored simple collection

--- run_simple_collection_community.sh
+++ run_simple_collection_community.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # TODO contributions -> contribs in anderen scripts
 # graph aufbau optimieren -> cocitation/bib coupling con igraph? https://gist.github.com/tts/4737987 http://igraph.org/python/doc/igraph.Graph-class.html#Incidence http://www.shizukalab.com/toolkits/sna/bipartite https://lists.nongnu.org/archive/html/igraph-help/2013-07/msg00014.html https://github.com/igraph/python-igraph/issues/168
@@ -24,7 +24,6 @@
 #   commclus: filtere mittels whitelist bei grapherzeugung?
 #   ich sollte erst filtern, wenn kein mm-dateizugriff mehr nöigt
 
-set -e  # Abbruch bei Fehler
 export DEBUG="DEBUG" # TODO produktiv raus
 PREFIX="simple-collection"
 COLL_PREFIX="collections/$PREFIX"
@@ -36,50 +35,62 @@ mkdir -p "output/stats"
 STATS_PREFIX="output/stats/$PREFIX"
 mkdir -p "output/logs"
 LOG_PREFIX="output/logs/$PREFIX"
-NAMESPACE_PREFIXES_FILE="output/$PREFIX-namespaces.txt"
+
+NAMESPACE_PREFIXES=output/$PREFIX-namespaces.txt
+HISTORY=$COLL_PREFIX-pages-meta-history.xml
+ID2AUTHOR=$CONTRIB_PREFIX-id2author.cpickle
+RAW_CONTRIBS=$CONTRIB_PREFIX-raw-contribs.mm
+TITLES=$CONTRIB_PREFIX.titles.cpickle
+ACC_CONTRIBS=$CONTRIB_PREFIX-acc-contribs.mm
+DOC_AUTH_CONTRIBS=$CONTRIB_PREFIX-doc-auth-contribs.mm
+AUTH_DOC_CONTRIBS=$CONTRIB_PREFIX-auth-doc-contribs.mm
+
+LOG_CONTRIBS=$LOG_PREFIX-contribs.log
 
 echo "generating XML dumps from JSON description"
-time python scripts/utils/generate_xml_from_simple_json_collection.py $PREFIX.json $COLL_PREFIX-articles.xml $COLL_PREFIX-pages-meta-history.xml
-bzip2 -zkf $COLL_PREFIX-articles.xml $COLL_PREFIX-pages-meta-history.xml 
+time python scripts/utils/generate_xml_from_simple_json_collection.py $PREFIX.json $COLL_PREFIX-articles.xml $HISTORY
+bzip2 -zkf $COLL_PREFIX-articles.xml $HISTORY 
 
 echo "extracting likely namespaces from XML dump"
 NS_MIN_OCCURENCES=1
-( time ./bash/get_likely_namespaces.sh $COLL_PREFIX-pages-meta-history.xml.bz2 $NS_MIN_OCCURENCES | tee $NAMESPACE_PREFIXES_FILE )|& tee $LOG_PREFIX-namespaces.log
+( time ./bash/get_likely_namespaces.sh $HISTORY.bz2 $NS_MIN_OCCURENCES | tee $NAMESPACE_PREFIXES )|& tee $LOG_PREFIX-namespaces.log
 
 echo "computing author contributions"
 CONTRIBUTION_VALUE=diff_numterms
 MIN_AUTH_DOCS=1
 MIN_DOC_AUTHS=1
-( time python scripts/history_to_contribs.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --id2author=$CONTRIB_PREFIX-id2author.cpickle.bz2 --contribs=$CONTRIB_PREFIX-raw-contribs.mm --contribution-value=$CONTRIBUTION_VALUE --min-auth-docs=$MIN_AUTH_DOCS --min-doc-auths=$MIN_DOC_AUTHS --namespace-prefixes=$NAMESPACE_PREFIXES_FILE ) |& tee $LOG_PREFIX-contribs.log
-python scripts/utils/binary_to_text.py gensim $CONTRIB_PREFIX-id2author.cpickle.bz2 $CONTRIB_PREFIX-id2author.txt # TODO produktiv raus
-mv $CONTRIB_PREFIX-raw-contribs.mm.metadata.cpickle $CONTRIB_PREFIX.titles.cpickle # Artikeltitel-Datei umbennen
-bzip2 -zf $CONTRIB_PREFIX-raw-contribs.mm $CONTRIB_PREFIX.titles.cpickle # komprimiere Beiträge, Artikeltitel
-bzip2 -dkf $CONTRIB_PREFIX-raw-contribs.mm.bz2  # TODO produktiv raus
-python scripts/utils/binary_to_text.py pickle $CONTRIB_PREFIX.titles.cpickle.bz2 $CONTRIB_PREFIX.titles.json # TODO produktiv raus
+( time python scripts/history_to_contribs.py --history-dump=$HISTORY.bz2 --id2author=$ID2AUTHOR.bz2 --contribs=$RAW_CONTRIBS --contribution-value=$CONTRIBUTION_VALUE --min-auth-docs=$MIN_AUTH_DOCS --min-doc-auths=$MIN_DOC_AUTHS --namespace-prefixes=$NAMESPACE_PREFIXES ) |& tee $LOG_CONTRIBS
+python scripts/utils/binary_to_text.py gensim $ID2AUTHOR.bz2 $CONTRIB_PREFIX-id2author.txt # TODO produktiv raus
+mv $RAW_CONTRIBS.metadata.cpickle $TITLES # Artikeltitel-Datei umbennen
+bzip2 -zf $RAW_CONTRIBS $TITLES # komprimiere Beiträge, Artikeltitel
+bzip2 -dkf $RAW_CONTRIBS.bz2  # TODO produktiv raus
+python scripts/utils/binary_to_text.py pickle $TITLES.bz2 $CONTRIB_PREFIX.titles.json # TODO produktiv raus
 
 echo "accmulating contributions"
-( time python scripts/accumulate_contribs.py --raw-contribs=$CONTRIB_PREFIX-raw-contribs.mm.bz2 --acc-contribs=$CONTRIB_PREFIX-acc-contribs.mm ) |& tee -a $LOG_PREFIX-contribs.log
+( time python scripts/accumulate_contribs.py --raw-contribs=$RAW_CONTRIBS.bz2 --acc-contribs=$ACC_CONTRIBS ) |& tee -a $LOG_CONTRIBS
 
 echo "thresholding contributions"
 MIN_CONTRIB_VALUE=1
-./bash/threshold_contribs.sh $CONTRIB_PREFIX-acc-contribs.mm $MIN_CONTRIB_VALUE > $CONTRIB_PREFIX-doc-auth-contribs.mm
-NUM_CONTRIBS_BEFORE=$(cat $CONTRIB_PREFIX-acc-contribs.mm | awk 'END {print NR-2}')
-NUM_CONTRIBS_AFTER=$(cat $CONTRIB_PREFIX-doc-auth-contribs.mm | awk 'END {print NR-2}')
-echo "thresholded number of contribs from $NUM_CONTRIBS_BEFORE to $NUM_CONTRIBS_AFTER" | tee -a $LOG_PREFIX-contribs.log
+./bash/threshold_contribs.sh $ACC_CONTRIBS $MIN_CONTRIB_VALUE > $DOC_AUTH_CONTRIBS
+NUM_CONTRIBS_BEFORE=$(cat $ACC_CONTRIBS | awk 'END {print NR-2}')
+NUM_CONTRIBS_AFTER=$(cat $DOC_AUTH_CONTRIBS | awk 'END {print NR-2}')
+echo "thresholded number of contribs from $NUM_CONTRIBS_BEFORE to $NUM_CONTRIBS_AFTER" | tee -a $LOG_CONTRIBS
 
 echo "transforming (docid,authorid,contribvalue) file to (authorid,docid,contribvalue) file"
-./bash/get_swapped_author_doc_contribs.sh $CONTRIB_PREFIX-doc-auth-contribs.mm > $CONTRIB_PREFIX-auth-doc-contribs.mm
-bzip2 -zf $CONTRIB_PREFIX-acc-contribs.mm $CONTRIB_PREFIX-doc-auth-contribs.mm $CONTRIB_PREFIX-auth-doc-contribs.mm
-bzip2 -dkf $CONTRIB_PREFIX-acc-contribs.mm.bz2 $CONTRIB_PREFIX-doc-auth-contribs.mm.bz2 $CONTRIB_PREFIX-auth-doc-contribs.mm.bz2 # TODO produktiv raus
+./bash/get_swapped_author_doc_contribs.sh $DOC_AUTH_CONTRIBS > $AUTH_DOC_CONTRIBS
+bzip2 -zf $ACC_CONTRIBS $DOC_AUTH_CONTRIBS $AUTH_DOC_CONTRIBS
+bzip2 -dkf $ACC_CONTRIBS.bz2 $DOC_AUTH_CONTRIBS.bz2 $AUTH_DOC_CONTRIBS.bz2 # TODO produktiv raus
 
 echo "creating graph from contributions"
-( time python scripts/contribs_to_graph.py --contribs=$CONTRIB_PREFIX-auth-doc-contribs.mm.bz2 --graph=$GRAPH_PREFIX-co-authorship.cpickle.gz ) |& tee $LOG_PREFIX-graph.log
+( time python scripts/contribs_to_graph.py --contribs=$AUTH_DOC_CONTRIBS.bz2 --graph=$GRAPH_PREFIX-co-authorship.cpickle.gz ) |& tee $LOG_PREFIX-graph.log
 
 echo "calculating graph stats"
 ( time python scripts/get_graph_stats.py --graph=$GRAPH_PREFIX-co-authorship.cpickle.gz ) |& tee -a $LOG_PREFIX-graph.log
 
+haschegif
+
 echo "calculating stats from history dump"
-( time python scripts/get_history_stats.py --history-dump=$COLL_PREFIX-pages-meta-history.xml.bz2 --stat-files-prefix=$STATS_PREFIX --namespace-prefixes=$NAMESPACE_PREFIXES_FILE ) |& tee $LOG_PREFIX-stats.log
+( time python scripts/get_history_stats.py --history-dump=$HISTORY.bz2 --stat-files-prefix=$STATS_PREFIX --namespace-prefixes=$NAMESPACE_PREFIXES ) |& tee $LOG_PREFIX-stats.log
 QUANTILE=1
 for STAT_FILE in $STATS_PREFIX*.csv; do
     [ -f "$STAT_FILE" ] || break

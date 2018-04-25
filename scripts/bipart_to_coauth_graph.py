64490728a5028dd1341811a7631bbae23b137a85
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-04-25 18:23:10 +0200

    comments

--- scripts/bipart_to_coauth_graph.py
+++ scripts/bipart_to_coauth_graph.py
@@ -25,16 +25,20 @@ def main():
     
     logger.info('running with:\n{}'.format(pformat({'input_bipart_path':input_bipart_path, 'output_coauth_graph_path':output_coauth_graph_path, 'mode':mode})))
     
+    # lade bipartiten Graph
     bipart_graph = Graph.Read_Picklez(input_bipart_path)
     logger.info('read bipartite graph')
     log_graph(bipart_graph)
     logger.info('{} document nodes, {} author nodes'.format(*get_bipartite_node_counts(bipart_graph)))
     
+    # erzeugt Co-Authorship-Graph der Dokumente
     calc_multiplicity = mode == 'mul'
     logger.info('applying bipartite projection, multiplicity? {}'.format(calc_multiplicity))
     coauth_graph = bipart_graph.bipartite_projection(types='type', multiplicity=calc_multiplicity, probe1=-1, which=0)
     logger.info('created co-authorship graph')
     log_graph(coauth_graph)
+    
+    # entferne einsame Knoten, 
     simplify_graph(coauth_graph)
     logger.info('simplified graph')
     log_graph(coauth_graph)

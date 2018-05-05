d4f766efe4ce247fac3188031b3678607cc854e3
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-05-05 12:30:00 +0200

    moved run scripts to bash

--- run_community.sh
+++ run_community.sh
@@ -22,10 +22,10 @@ fi
 CONTRIB_VALUES=(one diff_numterms)
 COAUTH_MODES=(mul jac)
 for CONTRIB_VALUE in "${CONTRIB_VALUES[@]}"; do
-    ./run_history_to_contribs.sh $PREFIX $CONTRIB_VALUE $TOP_N_CONTRIBS
+    ./bash/run_history_to_contribs.sh $PREFIX $CONTRIB_VALUE $TOP_N_CONTRIBS
     CPREFIX=$PREFIX-$CONTRIB_VALUE
     for COAUTH_MODE in "${COAUTH_MODES[@]}"; do 
-        ./run_contribs_to_graph.sh $CPREFIX $COAUTH_MODE $KEEP_MAX_EDGES
+        ./bash/run_contribs_to_graph.sh $CPREFIX $COAUTH_MODE $KEEP_MAX_EDGES
     done
 done
 

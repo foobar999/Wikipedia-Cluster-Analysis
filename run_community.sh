b9ed50669ea635a193cd024ec8ff529683b0d193
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-05-08 13:48:20 +0200

    community parameters to conf

--- run_community.sh
+++ run_community.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-# TODO zu verwendente Dinge parametrirbar  machen
 # TODO ich sollte das mit der max. komponente eher machen
 # TODO normierung raus, da winzige werte und newman eh nicht normiert?
 
@@ -14,18 +13,19 @@ source $CONFIG
 
 echo "PREFIX $PREFIX"
 echo "DEBUG $DEBUG"
+CONTRIB_VALUES=($CONTRIB_VALUES) # splitte String zu Array
+echo "CONTRIB_VALUES ${CONTRIB_VALUES[@]}"
 echo "TOP_N_CONTRIBS $TOP_N_CONTRIBS"
+COAUTH_MODES=($COAUTH_MODES)
+echo "COAUTH_MODES ${COAUTH_MODES[@]}"
 echo "KEEP_MAX_EDGES $KEEP_MAX_EDGES"
+COMM_METHODS=($COMM_METHODS)
+echo "COMM_METHODS ${COMM_METHODS[@]}"
 echo "USE_GIANT_COMP $USE_GIANT_COMP"
 if [ ! -z ${DEBUG+x} ]; then    # variable gesetzt?
     export DEBUG=$DEBUG
 fi
 
-#CONTRIB_VALUES=(one diff_numterms)
-CONTRIB_VALUES=(one )
-COAUTH_MODES=(mul jac coll)
-#COMM_METHODS=(greedy louvain)
-COMM_METHODS=(louvain)
 for CONTRIB_VALUE in "${CONTRIB_VALUES[@]}"; do
     ./bash/run_history_to_contribs.sh $PREFIX $CONTRIB_VALUE $TOP_N_CONTRIBS
     CPREFIX=$PREFIX-$CONTRIB_VALUE
@@ -40,3 +40,8 @@ for CONTRIB_VALUE in "${CONTRIB_VALUES[@]}"; do
 done
 
 
+##CONTRIB_VALUES=(one diff_numterms)
+#CONTRIB_VALUES=(one )
+#COAUTH_MODES=(mul jac coll)
+##COMM_METHODS=(greedy louvain)
+#COMM_METHODS=(louvain)

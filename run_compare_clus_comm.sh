6f39678cdd03d4451b3c058aed40070067a9d445
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-05-12 10:41:10 +0200

    comment out, added run_compare

---
+++ run_compare_clus_comm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+if (( $# != 1 )); then
+    echo "Usage: $0 CONFIG"
+    exit 1
+fi
+unset DEBUG
+CONFIG=$1
+source $CONFIG
+
+echo "PREFIX $PREFIX"
+echo "DEBUG $DEBUG"
+if [ ! -z ${DEBUG+x} ]; then # variable gesetzt?
+    export DEBUG=$DEBUG
+fi

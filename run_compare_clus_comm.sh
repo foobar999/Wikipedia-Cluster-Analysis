ca6166ba5c7ad414b2a04d4862f20d9e8f31acfc
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-05-12 11:55:56 +0200

    added comment

--- run_compare_clus_comm.sh
+++ run_compare_clus_comm.sh
@@ -31,11 +31,13 @@ for TOPIC_MODEL in "${TOPIC_MODELS[@]}"; do
     for CLUSTER_METHOD in "${CLUSTER_METHODS[@]}"; do
         for CLUSTER_NUM in "${CLUSTER_NUMS[@]}"; do
             CLUS_PREFIX=$PREFIX-$TOPIC_MODEL-$CLUSTER_METHOD-$CLUSTER_NUM
+            # Clustering Dokumenttitel -> Clusterlabel
             TITLECLUSTERS=output/clusters/$COMM_METHOD-titleclusters.json.bz2
             for CONTRIB_VALUE in "${CONTRIB_VALUES[@]}"; do
                 for COAUTH_MODE in "${COAUTH_MODES[@]}"; do 
                     for COMM_METHOD in "${COMM_METHODS[@]}"; do 
                         COMM_PREFIX=$PREFIX-$CONTRIB_VALUE-$COAUTH_MODE-$COMM_METHOD
+                        # Communities Dokumenttitel -> Communitylabel
                         TITLECOMMUNITIES=output/communities/$COMM_PREFIX-titlecommunities.json.bz2
                         echo "comparing $TCC_TITLECLUSTERS and $TITLECOMMUNITIES"
                     done

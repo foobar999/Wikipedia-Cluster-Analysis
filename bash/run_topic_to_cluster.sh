a6b6db8d6626d4649241f769494721f87a2187be
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-05-13 20:41:37 +0200

    added dense doc-top-matrix stuff to topic2cluster

--- bash/run_topic_to_cluster.sh
+++ bash/run_topic_to_cluster.sh
@@ -15,19 +15,19 @@ TM_PREFIX=output/topic/$IPREFIX
 CLUS_PREFIX=output/clusters/$OPREFIX
 LOG_PREFIX=output/logs/$OPREFIX
 
-BOW_MODEL=$BOW_CORPUS_PREFIX.mm
 BOW_TITLES=$BOW_CORPUS_PREFIX-titles.json
+DOCUMENT_TOPICS=$TM_PREFIX-document-topics.npz
 
 CLUSTER_LABELS=$CLUS_PREFIX.json
 TITLE_CLUSTER_LABELS=$CLUS_PREFIX-titleclusters.json
 LOG_CLUSTER=$LOG_PREFIX.log
 
 echo "computing $NUM_CLUSTERS clusters with $METHOD"
-(time python scripts/topic_to_cluster.py --bow=$BOW_MODEL.bz2 --tm=$TM_PREFIX --cluster-labels=$CLUSTER_LABELS --cluster-method=$METHOD --num-clusters=$NUM_CLUSTERS) |& tee $LOG_CLUSTER
+(time python scripts/topic_to_cluster.py --document-topics=$DOCUMENT_TOPICS --cluster-labels=$CLUSTER_LABELS --cluster-method=$METHOD --num-clusters=$NUM_CLUSTERS) |& tee $LOG_CLUSTER
+bzip2 -zf $CLUSTER_LABELS
 
 echo "evaluating clustering"
-(time python scripts/evaluate_dense_clustering.py --bow=$BOW_MODEL.bz2 --tm=$TM_PREFIX --cluster-labels=$CLUSTER_LABELS) |& tee -a $LOG_CLUSTER
-bzip2 -zf $CLUSTER_LABELS
+(time python scripts/evaluate_dense_clustering.py --document-topics=$DOCUMENT_TOPICS --cluster-labels=$CLUSTER_LABELS.bz2) |& tee -a $LOG_CLUSTER
 
 echo "generating documenttitle->clusterlabel mappings"
 python scripts/utils/get_title_communities.py --communities=$CLUSTER_LABELS.bz2 --titles=$BOW_TITLES.bz2 --titlecomms=$TITLE_CLUSTER_LABELS
--- scripts/evaluate_dense_clustering.py
+++ scripts/evaluate_dense_clustering.py
@@ -3,50 +3,41 @@ import argparse
 import logging
 import json
 from pprint import pformat
-from gensim.corpora import MmCorpus
-from gensim.models.ldamulticore import LdaMulticore
-from gensim.matutils import corpus2dense
-from gensim.utils import smart_open
 import numpy as np
-from utils.utils import init_logger
+from gensim.utils import smart_open
 from sklearn.metrics import silhouette_score, calinski_harabaz_score
+from utils.utils import init_logger, load_npz
  
 logger = init_logger()
 
 
 def main():
-    parser = argparse.ArgumentParser(description='calculates silhouette coefficient for a previous clustering')
-    parser.add_argument('--bow', type=argparse.FileType('r'), help='path to text-based input bow file (.mm/.mm.bz2)', required=True)
-    parser.add_argument('--tm', type=argparse.FileType('r'), help='gensim topic model prefix/path', required=True)
+    parser = argparse.ArgumentParser(description='calculates silhouette coefficient of a given clustering and its document-topic-matrix')
+    parser.add_argument('--document-topics', type=argparse.FileType('r'), help='path to input document-topic-file (.npz)', required=True)
     parser.add_argument('--cluster-labels', type=argparse.FileType('r'), help='path to input .json cluster labels file', required=True)
     
     args = parser.parse_args()
-    input_bow_path = args.bow.name
-    input_tm_path = args.tm.name
+    input_document_topics_path = args.document_topics.name
     input_cluster_labels_path = args.cluster_labels.name
     
-    logger.info('running with:\n{}'.format(pformat({'input_bow_path':input_bow_path, 'input_tm_path':input_tm_path, 'input_cluster_labels_path':input_cluster_labels_path})))
+    logger.info('running with:\n{}'.format(pformat({'input_document_topics_path':input_document_topics_path, 'input_cluster_labels_path':input_cluster_labels_path})))
     
-    logger.info('loading bow-corpus from {}'.format(input_bow_path))
-    bow = MmCorpus(input_bow_path)
-    logger.info('loaded bow-corpus {}'.format(bow))
-    logger.info('loading topic model from {}'.format(input_tm_path))
-    lda = LdaMulticore.load(input_tm_path)
-    logger.info('loaded topic model {}'.format(lda))
-    logger.info('combining both to dense document-topic-matrix')
-    dense_bow_topics = corpus2dense(lda[bow], lda.num_topics, bow.num_docs).T  # TODO das wird nicht gestreamt -> probleme?
-    logger.info('created dense matrix of shape {}'.format(dense_bow_topics.shape))
+    logger.info('loading dense document-topics from {}'.format(input_document_topics_path))
+    document_topics = load_npz(input_document_topics_path)
+    logger.info('loaded document-topics-matrix of shape {}'.format(document_topics.shape))
+    logger.debug('document-topics-matrix \n{}'.format(document_topics))
+    
     logger.info('loading cluster labels from {}'.format(input_cluster_labels_path))
-    with smart_open(input_cluster_labels_path, 'r') as ifile:
+    with smart_open(input_cluster_labels_path) as ifile:
         labels_list = json.load(ifile)
     logger.info('loaded {} labels'.format(len(labels_list)))
     logger.debug(labels_list)
     labels = np.asarray(labels_list)
     
     logger.info('calclating unsupervised evaluation metrics')
-    sil_score = silhouette_score(dense_bow_topics, labels, metric='euclidean') # groß=gut
+    sil_score = silhouette_score(document_topics, labels, metric='euclidean') # groß=gut
     logger.info('euclidean silhouette coefficient: {}'.format(sil_score))
-    ch_score = calinski_harabaz_score(dense_bow_topics, labels) # between-scatter durch within-scatter inkl. Straftermen -> groß=gut
+    ch_score = calinski_harabaz_score(document_topics, labels) # between-scatter durch within-scatter inkl. Straftermen -> groß=gut
     logger.info('calinski harabaz score: {}'.format(ch_score))
     
     

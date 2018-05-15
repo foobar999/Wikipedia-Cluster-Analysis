7a2c903e1d25ea88e824eaf21248fda330add68f
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-05-15 16:28:26 +0200

    removed edge pruning in coauth

--- run_community.sh
+++ run_community.sh
@@ -32,10 +32,10 @@ for CONTRIB_VALUE in "${CONTRIB_VALUES[@]}"; do
     TITLES=output/contribs/$CPREFIX-titles.json.bz2
     for COAUTH_MODE in "${COAUTH_MODES[@]}"; do 
         ./bash/run_contribs_to_graph.sh $CPREFIX $COAUTH_MODE $KEEP_MAX_EDGES
-        # CGPREFIX=$CPREFIX-$COAUTH_MODE
-        # for COMM_METHOD in "${COMM_METHODS[@]}"; do 
-            # ./bash/run_graph_to_community.sh $CGPREFIX $COMM_METHOD $USE_GIANT_COMP $TITLES
-        # done
+        CGPREFIX=$CPREFIX-$COAUTH_MODE
+        for COMM_METHOD in "${COMM_METHODS[@]}"; do 
+            ./bash/run_graph_to_community.sh $CGPREFIX $COMM_METHOD $USE_GIANT_COMP $TITLES
+        done
     done
 done
 

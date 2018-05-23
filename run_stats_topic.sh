00f26ef11a9b12fad831eaca18a08928f2e7d674
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-05-23 20:19:34 +0200

    added cluster plot gen to run_stats_topic

--- run_stats_topic.sh
+++ run_stats_topic.sh
@@ -10,6 +10,7 @@ source $CONFIG
 
 COLL_PREFIX=collections/$PREFIX
 TM_PREFIX=output/topic/$PREFIX
+CLUS_PREFIX=output/clusters/$PREFIX
 STATS_PREFIX=output/stats/$PREFIX
 
 echo "PREFIX $PREFIX"
@@ -20,6 +21,14 @@ fi
 echo "NO_BELOW $NO_BELOW"
 echo "NO_ABOVE $NO_ABOVE"
 echo "ARTICLE_MIN_TOKENS $ARTICLE_MIN_TOKENS"
+CLUSTER_METHODS=($CLUSTER_METHODS)
+echo "CLUSTER_METHODS ${CLUSTER_METHODS[@]}"
+CLUSTER_NUMS=($CLUSTER_NUMS) 
+echo "CLUSTER_NUMS ${CLUSTER_NUMS[@]}"
+EPSILONS=($EPSILONS)
+echo "EPSILONS ${EPSILONS[@]}"
+MIN_SAMPLES=($MIN_SAMPLES)
+echo "MIN_SAMPLES ${MIN_SAMPLES[@]}"
 
 ARTICLES_DUMP=$COLL_PREFIX-pages-articles.xml.bz2
 LOG_ART_STATS=$STATS_PREFIX-articles-stats.log
@@ -32,7 +41,25 @@ DOCUMENT_TOPICS=$TM_PREFIX-lda-document-topics.npz
 DOC_TOP_IMG=$STATS_PREFIX-lda-document-topics.pdf
 python3 scripts/get_document_viz.py --document-topics=$DOCUMENT_TOPICS --img-file=$DOC_TOP_IMG
 
-
+for CLUSTER_METHOD in "${CLUSTER_METHODS[@]}"; do
+    CMPREFIX=$CLUS_PREFIX-lda-$CLUSTER_METHOD
+    IMGPREFIX=$STATS_PREFIX-lda-$CLUSTER_METHOD
+    if [ $CLUSTER_METHOD == "dbscan" ]; then
+        for EPSILON in "${EPSILONS[@]}"; do
+            for MIN_SAMPLE in "${MIN_SAMPLES[@]}"; do
+                CLUS_FILE=$CMPREFIX-$EPSILON-$MIN_SAMPLE.json.bz2
+                IMG_FILE=$IMGPREFIX-$EPSILON-$MIN_SAMPLE.pdf
+                python3 scripts/get_document_clustering_viz.py --document-topics=$DOCUMENT_TOPICS --cluster-labels=$CLUS_FILE --img-file=$IMG_FILE
+            done
+        done
+    else
+        for CLUSTER_NUM in "${CLUSTER_NUMS[@]}"; do
+            CLUS_FILE=$CMPREFIX-$CLUSTER_NUM.json.bz2
+            IMG_FILE=$IMGPREFIX-$CLUSTER_NUM.pdf
+            python3 scripts/get_document_clustering_viz.py --document-topics=$DOCUMENT_TOPICS --cluster-labels=$CLUS_FILE --img-file=$IMG_FILE
+        done
+    fi
+done
 
 
 

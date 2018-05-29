1467748c9b870297a27224099de20c54540d55da
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-05-29 18:11:27 +0200

    added avg prob stat calc to run_stats

--- run_stats_topic.sh
+++ run_stats_topic.sh
@@ -9,6 +9,7 @@ CONFIG=$1
 source $CONFIG
 
 COLL_PREFIX=collections/$PREFIX
+BOW_PREFIX=output/bow/$PREFIX
 TM_PREFIX=output/topic/$PREFIX
 CLUS_PREFIX=output/clusters/$PREFIX
 STATS_PREFIX=output/stats/$PREFIX
@@ -30,20 +31,28 @@ echo "EPSILONS ${EPSILONS[@]}"
 MIN_SAMPLES=($MIN_SAMPLES)
 echo "MIN_SAMPLES ${MIN_SAMPLES[@]}"
 
-
+# Preprocessing-Auswirkungen
 ARTICLES_DUMP=$COLL_PREFIX-pages-articles.xml.bz2
 LOG_ART_STATS=$STATS_PREFIX-articles-stats.log
 NAMESPACE_PREFIXES=output/$PREFIX-namespaces.txt
 TOKEN_MIN_LEN=2
-python3 scripts/get_articles_stats.py --articles-dump=$ARTICLES_DUMP --no-below=$NO_BELOW --no-above=$NO_ABOVE --token-min-len=$TOKEN_MIN_LEN --article-min-tokens=$ARTICLE_MIN_TOKENS --namespace-prefixes=$NAMESPACE_PREFIXES |& tee  $LOG_ART_STATS
+python3 scripts/get_articles_stats.py --articles-dump=$ARTICLES_DUMP --no-below=$NO_BELOW --no-above=$NO_ABOVE --token-min-len=$TOKEN_MIN_LEN --article-min-tokens=$ARTICLE_MIN_TOKENS --namespace-prefixes=$NAMESPACE_PREFIXES |& tee $LOG_ART_STATS
 cat $LOG_ART_STATS | grep "stats\|density"
 
+# durchschnittliche Wahrscheinlichkeiten
+BOW=$BOW_PREFIX-bow.mm.bz2
+TOPIC_MODEL=$TM_PREFIX-lda
+LOG_TOPIC_FILE=$STATS_PREFIX-lda-topic-avg-probs.log
+python3 scripts/get_topic_stats.py --bow=$BOW --model-prefix=$TOPIC_MODEL |& tee $LOG_TOPIC_FILE
+
+# PCA, durchschn. W'keit-Plots
 DOCUMENT_TOPICS=$TM_PREFIX-lda-document-topics.npz
 DOC_DATA_IMG=$STATS_PREFIX-lda-document-data.pdf
 TOPIC_AVG_PROBS_IMG=$STATS_PREFIX-lda-topic-avg-probs.pdf
 TOPIC_AVG_PROBS_CDF_IMG=$STATS_PREFIX-lda-topic-probs-avg-cdf.pdf
 python3 scripts/get_document_viz.py --document-topics=$DOCUMENT_TOPICS --doc-data=$DOC_DATA_IMG --topic-avg-probs=$TOPIC_AVG_PROBS_IMG --topic-avg-probs-cdf=$TOPIC_AVG_PROBS_CDF_IMG
 
+# Cluster-Plots
 for CLUSTER_METHOD in "${CLUSTER_METHODS[@]}"; do
     CMPREFIX=$CLUS_PREFIX-lda-$CLUSTER_METHOD
     IMGPREFIX=$STATS_PREFIX-lda-$CLUSTER_METHOD

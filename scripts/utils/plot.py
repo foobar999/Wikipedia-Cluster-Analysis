a125679bc6558baa0f94e97f02963e6d6c0c4dc0
Your Name <you@example.com>
2018-06-05 20:02:23 +0200

    outcomment, reduced dpi

--- scripts/utils/plot.py
+++ scripts/utils/plot.py
@@ -9,10 +9,10 @@ logger = init_logger()
 
 def scatter_plot(data, ofpath, xlabel, ylabel):
     logger.info('plotting of shape {} to {}'.format(data.shape, ofpath))
-    plt.rc('font',family='Calibri')     
+    #plt.rc('font',family='Calibri')     
     plt.figure(figsize=(5,2.5))
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.scatter(np.arange(len(data)), data, c='dodgerblue', s=1)
-    plt.savefig(ofpath, bbox_inches='tight')
+    plt.scatter(np.arange(len(data)), data, c='dodgerblue', s=1, rasterized=True)
+    plt.savefig(ofpath, bbox_inches='tight', dpi=300)
        

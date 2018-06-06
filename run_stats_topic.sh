f95e3e2a051699a3f78ddc54ee66828217d3110b
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-06-06 20:23:31 +0200

    added cluster sizes plot

--- run_stats_topic.sh
+++ run_stats_topic.sh
@@ -14,24 +14,6 @@ TM_PREFIX=output/topic/$PREFIX
 CLUS_PREFIX=output/clusters/$PREFIX
 LOG_PREFIX=output/logs/$PREFIX
 
-STATS_PREPROP_DIR=output/stats/cluster_preprocessing
-mkdir -p $STATS_PREPROP_DIR
-STATS_PREPROP_PREFIX=$STATS_PREPROP_DIR/$PREFIX
-STATS_AVG_PREFIX=output/stats/cluster_avg
-mkdir -p $STATS_AVG_PREFIX
-STATS_AVG_PREFIX=$STATS_AVG_PREFIX/$PREFIX
-STATS_DOC_PLOTS_DIR=output/stats/cluster_doc_plots
-mkdir -p $STATS_DOC_PLOTS_DIR
-STATS_DOC_PLOTS_PREFIX=$STATS_DOC_PLOTS_DIR/$PREFIX
-CLUSTER_PLOTS_DIR=output/stats/cluster_plots
-mkdir -p $CLUSTER_PLOTS_DIR
-CLUSTER_PLOTS_PREFIX=$CLUSTER_PLOTS_DIR/$PREFIX
-STATS_SILHOUETTES_DIR=output/stats/cluster_silhouettes
-mkdir -p $STATS_SILHOUETTES_DIR
-STATS_SILHOUETTES_PREFIX=$STATS_SILHOUETTES_DIR/$PREFIX
-STATS_CENTRAL_DIR=output/stats/cluster_central
-mkdir -p $STATS_CENTRAL_DIR
-STATS_CENTRAL_PREFIX=$STATS_CENTRAL_DIR/$PREFIX
 
 echo "PREFIX $PREFIX"
 echo "DEBUG $DEBUG"
@@ -52,6 +34,9 @@ MIN_SAMPLES=($MIN_SAMPLES)
 echo "MIN_SAMPLES ${MIN_SAMPLES[@]}"
 
 # Preprocessing-Auswirkungen
+STATS_PREPROP_DIR=output/stats/cluster_preprocessing
+mkdir -p $STATS_PREPROP_DIR
+STATS_PREPROP_PREFIX=$STATS_PREPROP_DIR/$PREFIX
 # ARTICLES_DUMP=$COLL_PREFIX-pages-articles.xml.bz2
 # LOG_ART_STATS=$STATS_PREPROP_PREFIX-articles-stats.log
 # NAMESPACE_PREFIXES=output/$PREFIX-namespaces.txt
@@ -60,6 +45,9 @@ echo "MIN_SAMPLES ${MIN_SAMPLES[@]}"
 # cat $LOG_ART_STATS | grep "stats\|density" >> $LOG_ART_STATS
 
 # durchschnittliche Wahrscheinlichkeiten
+STATS_AVG_PREFIX=output/stats/cluster_avg
+mkdir -p $STATS_AVG_PREFIX
+STATS_AVG_PREFIX=$STATS_AVG_PREFIX/$PREFIX
 # BOW=$BOW_PREFIX-bow.mm.bz2
 # TOPIC_MODEL=$TM_PREFIX-lda
 # LOG_TOPIC_FILE=$STATS_AVG_PREFIX-lda-topic-avg-probs.log
@@ -71,6 +59,9 @@ DOCUMENT_TOPICS=$TM_PREFIX-lda-document-topics.npz
 # TOPIC_AVG_PROBS_CDF_IMG=$STATS_AVG_PREFIX-lda-topic-probs-avg-cdf.pdf
 # python3 -m scripts.stats.cluster.get_document_avg_viz --document-topics=$DOCUMENT_TOPICS --topic-avg-probs=$TOPIC_AVG_PROBS_IMG --topic-avg-probs-cdf=$TOPIC_AVG_PROBS_CDF_IMG
 
+STATS_DOC_PLOTS_DIR=output/stats/cluster_doc_plots
+mkdir -p $STATS_DOC_PLOTS_DIR
+STATS_DOC_PLOTS_PREFIX=$STATS_DOC_PLOTS_DIR/$PREFIX
 # 2D-Transformation
 DOCUMENTS_2D=$STATS_DOC_PLOTS_PREFIX-lda-documents-2d.npz
 #python3 -m scripts.stats.cluster.get_document_2d_transformed --document-topics=$DOCUMENT_TOPICS --documents-2d=$DOCUMENTS_2D
@@ -80,6 +71,9 @@ DOC_DATA_IMG=$STATS_DOC_PLOTS_PREFIX-lda-document-data.pdf
 #python3 -m scripts.stats.cluster.get_document_2d_viz --documents-2d=$DOCUMENTS_2D --img-file=$DOC_DATA_IMG 
 
 # 2D-Plot Cluster-gelabelte Dokumente
+CLUSTER_PLOTS_DIR=output/stats/cluster_plots
+mkdir -p $CLUSTER_PLOTS_DIR
+CLUSTER_PLOTS_PREFIX=$CLUSTER_PLOTS_DIR/$PREFIX
 #for CLUSTER_METHOD in "${CLUSTER_METHODS[@]}"; do
 #    CMPREFIX=$CLUS_PREFIX-lda-$CLUSTER_METHOD
 #    IMGPREFIX=$CLUSTER_PLOTS_PREFIX-lda-$CLUSTER_METHOD
@@ -101,6 +95,9 @@ DOC_DATA_IMG=$STATS_DOC_PLOTS_PREFIX-lda-document-data.pdf
 #done
 
 # silhouetten-plot
+STATS_SILHOUETTES_DIR=output/stats/cluster_silhouettes
+mkdir -p $STATS_SILHOUETTES_DIR
+STATS_SILHOUETTES_PREFIX=$STATS_SILHOUETTES_DIR/$PREFIX
 # nur Vielfache von 25, maximal 300
 for CLUSTER_METHOD in "${CLUSTER_METHODS[@]}"; do
     CLUSTER_LOG_PREFIX=$LOG_PREFIX-lda-$CLUSTER_METHOD-
@@ -112,7 +109,24 @@ for CLUSTER_METHOD in "${CLUSTER_METHODS[@]}"; do
     python3 -m scripts.stats.cluster.get_silhouette_plot --csv-data=$CLUSTER_SILHOUETTE_CSV --img-file=$CLUSTER_SILHOUETTE_PDF
 done
 
+# absteigende Clustergrößen
+STATS_CLUSTER_SIZES_DIR=output/stats/cluster_sizes
+mkdir -p $STATS_CLUSTER_SIZES_DIR
+STATS_CLUSTER_SIZES_PREFIX=$STATS_CLUSTER_SIZES_DIR/$PREFIX
+for CLUSTER_METHOD in "${CLUSTER_METHODS[@]}"; do
+   CMPREFIX=$CLUS_PREFIX-lda-$CLUSTER_METHOD
+   for CLUSTER_NUM in "${CLUSTER_NUMS[@]}"; do
+       CLUSTER_LABELS=$CMPREFIX-$CLUSTER_NUM.json.bz2
+       CLUSTER_SIZES_IMG=$STATS_CLUSTER_SIZES_PREFIX-lda-$CLUSTER_METHOD-$CLUSTER_NUM.pdf
+       python -m scripts.stats.cluster.get_cluster_stats --cluster-labels=$CLUSTER_LABELS --img=$CLUSTER_SIZES_IMG
+   done
+done
+
+
 # zentralste Dokumente je Cluster
+STATS_CENTRAL_DIR=output/stats/cluster_central
+mkdir -p $STATS_CENTRAL_DIR
+STATS_CENTRAL_PREFIX=$STATS_CENTRAL_DIR/$PREFIX
 #K=5
 #J=5
 #DOCUMENT_TOPICS=$TM_PREFIX-lda-document-topics.npz

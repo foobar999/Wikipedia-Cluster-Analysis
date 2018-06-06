815809ad93add7121e04472a8bacb3d3eca087d1
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-06-06 21:17:11 +0200

    plot params

--- scripts/utils/plot.py
+++ scripts/utils/plot.py
@@ -7,12 +7,12 @@ from scripts.utils.utils import init_logger
 logger = init_logger()
 
 
-def scatter_plot(data, ofpath, xlabel, ylabel, size=1, figsize=((5,2.5))):
+def scatter_plot(data, ofpath, xlabel, ylabel, rasterized=False, size=1, figsize=((5,2.5))):
     logger.info('plotting of shape {} to {}'.format(data.shape, ofpath))
     #plt.rc('font',family='Calibri')     
     plt.figure(figsize=figsize)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.scatter(np.arange(len(data)), data, c='dodgerblue', s=size, rasterized=True)
+    plt.scatter(np.arange(len(data)), data, c='dodgerblue', s=size, rasterized=rasterized)
     plt.savefig(ofpath, bbox_inches='tight', dpi=300)
        

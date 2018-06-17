b94d1f0593d6920c66869e7914df6141bf2042b2
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-06-17 10:44:42 +0200

    removed dbscan from run_topic_to_cluster

--- bash/run_topic_to_cluster.sh
+++ bash/run_topic_to_cluster.sh
@@ -1,30 +1,15 @@
 #!/bin/bash -e
 
-if [ $# != 4 ] && [ $# != 5 ]; then
-    echo "num args must be 4 or 5"
+if [ $# != 4 ]; then
+    echo "Usage: $0 IPREFIX BOW_CORPUS_PREFIX METHOD NUM_CLUSTERS"
     exit 1
 fi
 
 IPREFIX=$1
 BOW_CORPUS_PREFIX=$2
 METHOD=$3
-if [ $METHOD == "dbscan" ]; then
-    if [ $# != 5 ]; then
-        echo "Usage: $0 IPREFIX BOW_CORPUS_PREFIX dbscan EPSILON MIN_SAMPLES"
-        exit 1
-    fi
-    EPSILON=$4
-    MIN_SAMPLES=$5
-    OPREFIX=$IPREFIX-$METHOD-$EPSILON-$MIN_SAMPLES
-else
-    if [ $# != 4 ]; then
-        echo "Usage: $0 IPREFIX BOW_CORPUS_PREFIX METHOD NUM_CLUSTERS"
-        exit 1
-    fi
-    NUM_CLUSTERS=$4
-    OPREFIX=$IPREFIX-$METHOD-$NUM_CLUSTERS
-fi
-   
+NUM_CLUSTERS=$4
+OPREFIX=$IPREFIX-$METHOD-$NUM_CLUSTERS   
     
 TM_PREFIX=output/topic/$IPREFIX
 CLUS_PREFIX=output/clusters/$OPREFIX
@@ -37,13 +22,8 @@ CLUSTER_LABELS=$CLUS_PREFIX.json
 TITLE_CLUSTER_LABELS=$CLUS_PREFIX-titleclusters.json
 LOG_CLUSTER=$LOG_PREFIX.log
 
-if [ $METHOD == "dbscan" ]; then
-    ARGS=" --eps=$EPSILON --min-samples=$MIN_SAMPLES "
-else
-    ARGS=" --num-clusters=$NUM_CLUSTERS "
-fi
 echo "computing clusters with $METHOD"
-(time python3 -m scripts.cluster.topic_to_cluster --document-topics=$DOCUMENT_TOPICS --cluster-labels=$CLUSTER_LABELS --cluster-method=$METHOD $ARGS) |& tee $LOG_CLUSTER
+(time python3 -m scripts.cluster.topic_to_cluster --document-topics=$DOCUMENT_TOPICS --cluster-labels=$CLUSTER_LABELS --cluster-method=$METHOD --num-clusters=$NUM_CLUSTERS) |& tee $LOG_CLUSTER
 bzip2 -zf $CLUSTER_LABELS
 
 echo "evaluating clustering"

be8848a9477bb564d53a34148eb57e358e700b1b
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-07-17 15:28:56 +0200

    simplified get_title_communities

--- scripts/utils/get_title_communities.py
+++ scripts/utils/get_title_communities.py
@@ -1,16 +1,12 @@
-import os, sys
-import logging
 import argparse
-import json
-import bz2
 from pprint import pformat
-from scripts.utils.utils import init_logger
+from scripts.utils.utils import init_logger, load_communities, load_titles, save_data_to_json
 
 logger = init_logger()
              
      
 def main():
-    parser = argparse.ArgumentParser(description='maps a given communities file with document labels and a give binary titles file with document titles to a doctitle->communitylabel file')
+    parser = argparse.ArgumentParser(description='maps a given communities/clustering file with document labels and a given metadata file with document titles to a doctitle->communitylabel file')
     parser.add_argument('--communities', type=argparse.FileType('r'), help='path to input .json.bz2 communities file', required=True)
     parser.add_argument('--titles', type=argparse.FileType('r'), help='path to input .json.bz2 titles file', required=True)
     parser.add_argument('--titlecomms', type=argparse.FileType('w'), help='path to output doctitle->communitylabel .json file', required=True)
@@ -22,10 +18,8 @@ def main():
     
     logger.info('running with:\n{}'.format(pformat({'input_communities_path':input_communities_path, 'input_titles_path':input_titles_path, 'output_titlecomms_path':output_titlecomms_path})))
     
-    with bz2.open(input_titles_path, 'rt') as input_titles_file:
-        titles = json.load(input_titles_file)
-    with bz2.open(input_communities_path, 'rt') as input_communities_file:
-        communities = json.load(input_communities_file)
+    titles = load_titles(input_titles_path)
+    communities = load_communities(input_communities_path)
         
     logger.info('{} titles'.format(len(titles)))
     logger.debug('titles \n{}'.format(titles))
@@ -36,13 +30,11 @@ def main():
         title_communities = {titles[doc_id[1:]]: comm_label for doc_id,comm_label in communities.items()}
     else:
         title_communities = {titles[str(doc_id)]: comm_label for doc_id,comm_label in enumerate(communities) if comm_label >= 0}
-    logger.info('generated {} title_communities and removed noise'.format(len(title_communities)))
+    logger.info('generated {} title_communities'.format(len(title_communities)))
     logger.debug('title_communities \n{}'.format(title_communities))
         
-    logger.info('saving to {}'.format(output_titlecomms_path))
-    with open(output_titlecomms_path, 'w') as output_titlecomms_file:
-        json.dump(title_communities, output_titlecomms_file, indent=1)
-        
+    logger.info('saving title communities')
+    save_data_to_json(title_communities, output_titlecomms_path)
         
 if __name__ == '__main__':
     main()

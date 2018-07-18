eb2d2c3758897840411b9c93ed6deda3c5270e78
DieGabelLinksVomTeller <thomas.schell35@web.de>
2018-07-18 14:28:19 +0200

    removed some unnecessary imports

--- scripts/stats/cluster/get_topic_stats.py
+++ scripts/stats/cluster/get_topic_stats.py
@@ -1,6 +1,4 @@
-import os, sys
 import argparse
-import logging
 from collections import defaultdict
 from pprint import pformat
 from gensim import matutils
@@ -18,8 +16,9 @@ def get_topic_terms(ldamodel, topicid, topn=10):
     bestn = matutils.argsort(topic, topn, reverse=True)
     return [(idx, topic[idx]) for idx in bestn]
 
+    
 def main():
-    parser = argparse.ArgumentParser(description='applies a trained lda model to a bag-of-words and saves the resulting corpus topics as a binary numpy dense matrix file (rows=documents, cols=topics)')
+    parser = argparse.ArgumentParser(description='prints the topics of a LDA instance, sorted decreasing by thei average probabilities in the collection; prints some stats of the appereances of the most important terms of the most important topics')
     parser.add_argument('--bow', type=argparse.FileType('r'), help='path to input bow file (.mm/.mm.bz2)', required=True)
     parser.add_argument('--model-prefix', type=argparse.FileType('r'), help='prefix of input binary lda model files', required=True)
     
@@ -33,14 +32,16 @@ def main():
     bow = MmCorpus(input_bow_path)
     logger.info('loading topic model from {}'.format(input_model_prefix))
     model = SaveLoad.load(input_model_prefix)
+    logger.info('loaded {} docs, {} topics'.format(bow.num_docs, model.num_topics))
     
-    logger.info('{} docs, {} topics'.format(bow.num_docs, model.num_topics))
+    logger.info('generating sparse document-topic-matrix')
     document_topic_probs = dok_matrix((bow.num_docs, model.num_topics), dtype='d')
     for docid, topics in enumerate(model[bow]):
         for topicid, prob in topics:
             document_topic_probs[docid,topicid] = prob
     document_topic_probs = document_topic_probs.tocsr()
     
+    logger.info('calculating average topic probabilities of the document collection')
     topics_avg = document_topic_probs.sum(axis=0) / bow.num_docs
     topics_max = document_topic_probs.max(axis=0).todense().tolist()[0]
     logger.debug('avg {} shape {}'.format(topics_avg, topics_avg.shape))
@@ -49,9 +50,9 @@ def main():
     topics_avg.sort(key=lambda t:t[1], reverse=True)
     
     num_printed_terms = 10
-    logger.info('my most important topics')
+    logger.info('topics with highest average probabilities')
     for topicid, topic_avg_prob in topics_avg:
-        logger.info('topic ID={0}, prob={1:.4f}, maxprob={2:.4f}, terms:\n{3}'.format(topicid, topic_avg_prob, topics_max[topicid], model.print_topic(topicid, topn=num_printed_terms)))
+        logger.info('topic ID={0}, avgprob={1:.4f}, maxprob={2:.4f}, terms:\n{3}'.format(topicid, topic_avg_prob, topics_max[topicid], model.print_topic(topicid, topn=num_printed_terms)))
         
     num_top_topics = min(5, len(topics_avg))
     num_top_terms = 20
@@ -59,7 +60,6 @@ def main():
     logger.info('calculating stats of top-{}-topics {} with top-{}-terms per topic'.format(num_top_topics, top_topics, num_top_terms))
     term_topics = defaultdict(list) # mapping termid->topicids für alle termids, die in top-k von irgendwelchen topics enthalten
     for topicid in top_topics:
-        #for termid, prob in model.get_topic_terms(topicid, topn=num_top_terms):
         for termid, prob in get_topic_terms(model, topicid, topn=num_top_terms):
             term_topics[termid].append(topicid)
     term_topics = dict(term_topics)
